1ef98fe233b6e0230e96a60aa08d7d5e0828b1cb
KK Tech <dyxj6n786p@privaterelay.appleid.com>
2026-04-22 17:38:45 +0100

    I have fixed some small issues in my code following the feedback instructions

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -12,8 +12,13 @@ window.addEventListener("load", function (e) {
 
 function populateStorage() {
   if (myLibrary.length == 0) {
- const book1 = new Book("Robison Crusoe", "Daniel Defoe", 252, true);
- const book2 = new Book( "The Old Man and the Sea", "Ernest Hemingway",127,true);
+    const book1 = new Book("Robison Crusoe", "Daniel Defoe", 252, true);
+    const book2 = new Book(
+      "The Old Man and the Sea",
+      "Ernest Hemingway",
+      127,
+      true
+    );
     myLibrary.push(book1);
     myLibrary.push(book2);
   }
@@ -22,20 +27,24 @@ function populateStorage() {
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function submit() {
+  const trimmedTitle = titleInput.value.trim();
+  const trimmedAuthor = authorInput.value.trim();
+  const pageCount = Number(pagesInput.value);
+
   if (
-    authorInput.value.trim() == "" ||
-    titleInput.value.trim() == "" ||
+    trimmedTitle == "" ||
+    trimmedAuthor == "" ||
     pagesInput.value == "" ||
-    Number(pagesInput.value) <= 0 ||
-    isNaN(Number(pagesInput.value))
+    pageCount <= 0 ||
+    isNaN(pageCount)
   ) {
     alert("Please fill all fields!");
     return false;
   } else {
     const book = new Book(
-      titleInput.value,
-      authorInput.value,
-      Number(pagesInput.value),
+      trimmedTitle,
+      trimmedAuthor,
+      pageCount,
       checkInput.checked
     );
     myLibrary.push(book);
